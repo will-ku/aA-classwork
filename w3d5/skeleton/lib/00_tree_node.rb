ab785a6ac892ff58796d7ecae2aaec9a448c7199
moustafa <mogarcia626@gmail.com>
2021-04-02 15:11:20 -0400

    Started PolyTreeNode

--- w3d5/skeleton/lib/00_tree_node.rb
+++ w3d5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,24 @@
 class PolyTreeNode
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(parent_node)
+        @parent = parent_node
+        if !parent_node.children.include?(self) && parent_node.parent != nil
+            node.children << self
+        end
+    end
+
+
+
+    
+
+
+    
 
 end
