da2200008dc6f683189595ca179d4cc7f5bb299a
Elijah Ally <elijahally18@gmail.com>
2021-04-09 16:59:42 -0400

    added #largest_contig_sum

--- w4d5/exectime.rb
+++ w4d5/exectime.rb
@@ -7,7 +7,7 @@ my_min(list)  # =>  -5
 
 Phase I
 
-First, write a function that compares each element to every other element of the 
+First, write a function that compares each element to every other element of the q
 list. Return the element if all other elements in the array are larger.
 What is the time complexity for this function?
  
@@ -43,35 +43,62 @@ end
 
 # Largest Contiguous Sub-sum
 
+# def largest_contiguous_subsum(list)
+#     outer_arr = []
+#     (0...list.length).each do |i|
+#         sub_arr = [list[i]]
+#         outer_arr << sub_arr
+#         new_arr = sub_arr.dup
+#         (i+1...list.length).each do |j|
+#             new_arr << list[j]
+#             outer_arr << new_arr
+#         end
+#     end
+#     p outer_arr
+#     outer_arr.map! {|arr| arr.sum}
+#     p outer_arr
+#     return outer_arr.max.to_i
+# end
+
+#polynomial
+# def largest_contiguous_subsum(list)
+#     outer_arr = [] 
+
+#     (0...list.length).each do |i| 
+#         outer_arr << [list[i]]
+#         (i+1...list.length).each do |j| 
+#             outer_arr << list[i..j]
+#         end
+#     end
+#
+ #   
+#     outer_arr.map! { |arr| arr.sum } 
+#     outer_arr.max
+# end 
+
+
 def largest_contiguous_subsum(list)
-    outer_arr = []
-    (0...list.length).each do |i|
-        sub_arr = [list[i]]
-        outer_arr << sub_arr
-        new_arr = sub_arr.dup
-        (i+1...list.length).each do |j|
-            new_arr << list[j]
-            outer_arr << new_arr
+    largest_sum = 0
+    current = 0
+
+    list.each.with_index do |num, i|
+        current += num
+        if  current > largest_sum
+            largest_sum = current
+        else
+            current = 0
         end
     end
-    p outer_arr
-    outer_arr.map! {|arr| arr.sum}
-    p outer_arr
-    return outer_arr.max.to_i
 
+    largest_sum
 end
-# [5, 3, -7], 
-# [5, 3, -7], 
-# [5, 3, -7], 
-# [3, -7], 
-# [3, -7], 
-# [-7]]
-# [1, 1, 1, -4, -4, -7]
+
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
 
 
 list = [5, 3, -7]
-p largest_contiguous_subsum(list) # => 8
-
+largest_contiguous_subsum(list) # => 8
     # # possible sub-sums
     # [5]           # => 5
     # [5, 3]        # => 8 --> we want this one
