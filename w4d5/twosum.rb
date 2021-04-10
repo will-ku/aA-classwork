14152a35b5caff1d15b43aa1d8f8ae94cb18bd7e
Elijah Ally <elijahally18@gmail.com>
2021-04-09 20:02:14 -0400

    changed two_sum

--- w4d5/twosum.rb
+++ w4d5/twosum.rb
@@ -8,6 +8,23 @@ def bad_two_sum?(arr, target_sum)
     false
 end
 
-arr = [0, 1, 5, 7]
-p two_sum?(arr, 6) # => should be true
-p two_sum?(arr, 10) # => should be false
+
+def okay_two_sum?(arr, target)
+    arr.sort!
+    mid = arr.length/2
+    side1 = arr.take(mid) # [0, 1]
+    side2 = arr.drop(mid) #[5, 7, 8]
+
+    # current = arr.first
+
+    # (1...arr.length).each do |i|
+    #     current += arr[i]
+    #     return true if target == current
+    #     current = arr[i]
+    # end
+
+end
+
+arr = [1, 2, 7, 5]
+p okay_two_sum?(arr, 6) # => should be true
+p okay_two_sum?(arr, 10) # => should be false
