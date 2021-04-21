6f7854a081447a87199baefb78aec51a40eb635e
will-ku <william.ku9192@gmail.com>
2021-04-20 21:26:26 -0400

    w6d2

---
+++ w6d2/lib/01_sql_object.rb
@@ -0,0 +1,58 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    # ...
+  end
+
+  def self.finalize!
+  end
+
+  def self.table_name=(table_name)
+    self.name.tableize = table_name.tableize
+  end
+
+  def self.table_name
+    @table_name = self.name.tableize
+    # self.name.underscore.pluralize
+  end
+
+  def self.all
+    # ...
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    # ...
+  end
+
+  def attributes
+    # ...
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
