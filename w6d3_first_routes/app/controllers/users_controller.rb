52338fa79ed2d3bfe5ed64d0f05a8abcc81bfa86
will-ku <william.ku9192@gmail.com>
2021-04-21 19:53:52 -0400

    Created User#create

--- w6d3_first_routes/app/controllers/users_controller.rb
+++ w6d3_first_routes/app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        users = User.all
+        render json: users
+    end
+
+    def create
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save
+            render json: user
+        end
+    end
+
+    def show
+        render json: params
     end
 end
