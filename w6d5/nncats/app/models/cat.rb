6321a991416649155b83c7a34579fa26e292eed1
will-ku <william.ku9192@gmail.com>
2021-04-23 16:32:57 -0400

    w6d5

---
+++ w6d5/nncats/app/models/cat.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Cat < ApplicationRecord
+    validates :birth_date, :description, presence: true
+    validates :name, uniqueness: true
+    validates :sex, inclusion: {:in => ['m','f']}
+end
