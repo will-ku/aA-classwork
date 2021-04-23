51c66a414cb3c1a7720431fa8b6f3f6a19fd9958
will-ku <william.ku9192@gmail.com>
2021-04-23 17:46:58 -0400

    Created new/create form

--- w6d5/nncats/app/controllers/cats_controller.rb
+++ w6d5/nncats/app/controllers/cats_controller.rb
@@ -8,4 +8,24 @@ class CatsController < ApplicationController
     @cat = Cat.find_by(id: params[:id]) 
     render :show 
   end
+
+  def new
+    @cat = Cat.new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end    
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
+  end
+
 end
