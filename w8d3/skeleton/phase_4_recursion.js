7e7f9a9b2f1947d7ae8b6a224f98da8b768e9eed
Inho Lee <inhojl@outlook.com>
2021-05-05 17:22:29 -0700

    finished bsearch

--- w8d3/skeleton/phase_4_recursion.js
+++ w8d3/skeleton/phase_4_recursion.js
@@ -14,3 +14,74 @@ function range(start,end) {
     
     return [start].concat(range(start+1,end))
 }
+
+
+
+function sumRec(arr) {
+
+  if (arr.length === 0) return 0;
+
+  return arr[0] + sumRec(arr.slice(1, arr.length));
+
+}
+
+
+// 2 ^ 1
+function exponent(base, exp) {
+  if (exp === 0) return 1;
+  return base * exponent(base, exp-1);
+}
+
+// n = 4
+// 1,1,2,3,5
+function fibonacci(n) {
+  // base cases
+  if (n === 1) return [ 1 ];
+  if (n === 2) return [ 1,1 ];
+  // n === 3
+  // recursive case
+  const prevFibSequence = fibonacci(n-1);
+  let nextFibNum = prevFibSequence
+                        .slice(-2)
+                        .reduce((acc, el) => acc + el, 0)
+
+  return prevFibSequence.concat([nextFibNum])
+}
+
+// []
+// [1]
+// [1,2]
+// [1,[2]]
+
+function deepDup(arr) {
+  const dup = []
+  arr.forEach((el) => {
+    if (Array.isArray(el)) {
+      dup.push(deepDup(el))
+    } else {
+      dup.push(el)
+    }
+  })
+  return dup;
+}
+
+function bsearch(arr, target) {
+  if (arr.length === 0) return -1
+
+  const middle = Math.floor(arr.length / 2);
+
+  // 1. if middle is target
+  if (arr[middle] === target) {
+    return middle;
+  } else if (target < arr[middle]) {
+
+    return bsearch(arr.slice(0, middle), target)
+  } else if (target > arr[middle]) {
+    // 5
+    let offset = bsearch(arr.slice(middle+1, arr.length), target)
+    // offset 2
+    if (offset === -1) return offset
+    return middle + offset + 1
+  }
+
+}
