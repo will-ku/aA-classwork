afaeca0197961a4f404a5d7f3bfd28e55103a346
dennis-25 <dennisyu225@gmail.com>
2021-05-13 14:15:47 -0700

    start twitter

--- w9d4/frontend/follow_toggle.js
+++ w9d4/frontend/follow_toggle.js
@@ -3,8 +3,22 @@ class FollowToggle {
     constructor($el) {
         this.userId = $el.data('user-id')
         this.followState = $el.data('initial-follow-state')
+        this.render();
+        this.handleClick();
     }
 
+    render() {
+      if (this.followState === 'unfollowed') {
+        return $(button).text('Follow!');
+      } 
+      if (this.followState === 'followed!') {
+        return $(button).text('Unfollow!');
+      } 
+    }
+
+    handleClick() {
+
+    }
 
 }
 
