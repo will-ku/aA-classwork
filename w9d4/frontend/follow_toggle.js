5be5ee14bddd6398ab1b4d791dc0179fde07ba4a
will-ku <william.ku9192@gmail.com>
2021-05-13 16:40:58 -0400

    Completed constructor function

---
+++ w9d4/frontend/follow_toggle.js
@@ -0,0 +1,11 @@
+
+class FollowToggle {
+    constructor($el) {
+        this.userId = $el.data('user-id')
+        this.followState = $el.data('initial-follow-state')
+    }
+
+
+}
+
+module.exports = FollowToggle;
