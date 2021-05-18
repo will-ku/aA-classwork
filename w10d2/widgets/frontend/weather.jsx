6d768b39898f8a42b5330778ab0fdfbc677c173b
will-ku <william.ku9192@gmail.com>
2021-05-18 19:53:59 -0400

    Updated weather API

--- w10d2/widgets/frontend/weather.jsx
+++ w10d2/widgets/frontend/weather.jsx
@@ -13,6 +13,27 @@ class Weather extends React.Component {
         </div>
       )
     }
+    
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(this.pollWeather);
+    }
+
+    pollWeather() {
+	    let url = 'api.openweathermap.org/data/2.5/weather?lat=40.6025134&lon=-74.00943330000001&appid=f816d7f39052e3a98b21952097a43076';
+      let xmlhttp = new XMLHttpRequest();
+        request = new XMLHttpRequest();
+        request.open('POST', url, true);
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+        request.send(data);
+        xmlhttp.send();
+    }
+
 }
 
+
 export default Weather;
+
+// lattitude: 40.6025134
+// longitude: -74.00943330000001
+
+// api.openweathermap.org/data/2.5/weather?lat=40.6025134&lon=-74.00943330000001&appid=f816d7f39052e3a98b21952097a43076
