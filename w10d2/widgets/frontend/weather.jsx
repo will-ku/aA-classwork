15f5a9b07c4644a7adcd2abd173ad68696a62823
Roger Yang <roger.h.yang@gmail.com>
2021-05-18 17:09:33 -0700

    finish weather

--- w10d2/widgets/frontend/weather.jsx
+++ w10d2/widgets/frontend/weather.jsx
@@ -4,30 +4,50 @@ import React from 'react';
 class Weather extends React.Component {
     constructor(props) {
       super(props)
+      this.state = {
+          data: null
+      }
     }
 
     render() {
-      return (
-        <div>
-          <h1>This is weather.</h1>
-        </div>
-      )
+        let weather = null;
+        if (this.state.data) {
+            console.log(this.state.data);
+            return (
+                <p>Temperature: {this.state.data.main.temp}°K</p>
+            )
+        }
+        return (
+            <div>
+            <h1>This is weather.</h1>
+            {weather}
+            </div>
+        )
     }
     
     componentDidMount() {
-        navigator.geolocation.getCurrentPosition(this.pollWeather);
+        navigator.geolocation.getCurrentPosition(this.pollWeather.bind(this));
     }
 
-    pollWeather() {
-	    let url = 'api.openweathermap.org/data/2.5/weather?lat=40.6025134&lon=-74.00943330000001&appid=f816d7f39052e3a98b21952097a43076';
-      let xmlhttp = new XMLHttpRequest();
-        request = new XMLHttpRequest();
-        request.open('POST', url, true);
-        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-        request.send(data);
-        xmlhttp.send();
+    pollWeather(pos) {
+        let lat = pos.coords.latitude;
+        let lng = pos.coords.longitude;
+        const apikey = 'f816d7f39052e3a98b21952097a43076';
+	    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${apikey}`;
+        let request = new XMLHttpRequest();
+        let that = this;
+        request.open('GET', url, true);
+        request.onload = function() {
+            if (this.status >= 200) {
+                that.setState({
+                    data: JSON.parse(this.response)
+                });
+            } else {
+                console.log(this.status);
+            }
+        };
+        request.send();
     }
-
 }
 
 
