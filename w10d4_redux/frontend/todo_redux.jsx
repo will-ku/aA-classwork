7b9d6ed9cbfc5b0d7fa1a63f65512e3f3a539911
Schploink <thekpoc@gmail.com>
2021-05-21 12:19:43 -0700

    attempt to test reducers on window)

--- w10d4_redux/frontend/todo_redux.jsx
+++ w10d4_redux/frontend/todo_redux.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Redux from 'redux';
-import {receiveTodo} from '../actions/todo_actions'
+import {receiveTodo, receiveTodos} from './actions/todo_actions'
+
+const store = configureStore()
+window.store = store
+const object = { id: 3, title: "New Todo" }
+store.dispatch(receiveTodo(object)) 
+store.dispatch(receiveTodos(object)) 
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const header = <h1>Todos App</h1>
   ReactDOM.render(header, document.getElementById("content"))
-  const store = configureStore()
-  window.store = store
-  console.log(store.getState())
 
 })
