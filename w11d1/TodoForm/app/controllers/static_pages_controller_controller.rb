5511a26fdaa27c02dc67cf6c81b761ddacaf297d
Schploink <thekpoc@gmail.com>
2021-05-24 16:45:43 -0700

    rails stuff did

---
+++ w11d1/TodoForm/app/controllers/static_pages_controller_controller.rb
@@ -0,0 +1,6 @@
+class StaticPagesControllerController < ApplicationController
+
+  def root
+    render :root
+  end
+end
---
+++ w11d1/TodoForm/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  
+  namespace :api, defaults: {format: :json} do 
+    resources :todos, only: [:index, :show, :create, :destroy, :update]
+
+  end
+
+  root to: 'static_pages#root'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
