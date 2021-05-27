490c17a16cf9dcfb33dc68e5d9c81486bcb10326
will-ku <william.ku9192@gmail.com>
2021-05-27 16:47:24 -0400

    completed front end auth

---
+++ frontend_auth/frontend/components/session/signup.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+class Signup extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      email: '',
+      password: ''
+    }
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+
+}
+
+export default Signup
